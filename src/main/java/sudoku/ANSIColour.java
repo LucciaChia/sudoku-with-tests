3e2f04f428493f54fcdb5bcdb3c4e6538819b518
boris.krul@exxeta.sk <boris.krul@exxeta.sk>
2019-05-06 13:00:25 +0200

    remove unneeded final and static in ANSIColour #8

--- src/main/java/sudoku/ANSIColour.java
+++ src/main/java/sudoku/ANSIColour.java
@@ -1,10 +1,10 @@
 package sudoku;
 
 public interface ANSIColour {
-    static final String ANSI_RESET = "\u001B[0m";
-    static final String ANSI_RED = "\u001B[31m";
-    static final String ANSI_GREEN = "\u001B[32m";
-    static final String ANSI_PURPLE = "\u001B[35m";
-    static final String ANSI_BOLD = "\u001B[1m";
+    String ANSI_RESET = "\u001B[0m";
+    String ANSI_RED = "\u001B[31m";
+    String ANSI_GREEN = "\u001B[32m";
+    String ANSI_PURPLE = "\u001B[35m";
+    String ANSI_BOLD = "\u001B[1m";
 
 }
