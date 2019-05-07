7017d3ccff4a4267ac9e1eb86205369ddcd6113f
boris.krul@exxeta.sk <boris.krul@exxeta.sk>
2019-05-07 10:23:48 +0200

    add comments for Command and CommandPicker #11

--- src/main/java/sudoku/command/Command.java
+++ src/main/java/sudoku/command/Command.java
@@ -2,6 +2,15 @@ package sudoku.command;
 
 import sudoku.model.Sudoku;
 
+/**
+ * Command runs given strategy on sudoku to achieve next state
+ */
 public interface Command {
+
+    /**
+     * Given strategy will be executed and returns sudoku that should be closer to solution
+     *
+     * @return      Sudoku that is next state achieved by applying chosen strategy
+     */
     Sudoku execute();
 }
--- src/main/java/sudoku/command/CommandPicker.java
+++ src/main/java/sudoku/command/CommandPicker.java
@@ -9,18 +9,33 @@ import sudoku.strategy.Resolvable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
+/**
+ * CommandPicker runs given strategy on sudoku to achieve next state
+ */
 @Getter
 public class CommandPicker implements Command {
 //    Resolvable resolvable;
 //    Sudoku sudoku;
     private List<Step> stepList = new ArrayList<>();
 
+    /**
+     * Constructor method that is given current state of sudoku and strategy that is to be used in next step
+     *
+     * @param resolvable    resolvable that is strategy chosen to be used in this step
+     * @param sudoku        sudoku that is the current state
+     */
     public CommandPicker(Resolvable resolvable, Sudoku sudoku) {
 //        this.resolvable = resolvable;
 //        this.sudoku = sudoku;
         stepList.add(new OneChangeStep(resolvable, sudoku));
     }
 
+    /**
+     * Choosen strategy will be executed and returns sudoku that should be closer to solution
+     *
+     * @return      Sudoku that is next state achieved by applying chosen strategy
+     */
     @Override
     public Sudoku execute() {
         Sudoku sudoku = ((OneChangeStep) Objects.requireNonNull(getLastStep())).getSudoku();
@@ -35,6 +50,11 @@ public class CommandPicker implements Command {
         }
     }
 
+    /**
+     * Method that prints name of the chosen strategy and current state of the sudoku
+     *
+     * @return      a string containing strategy name and sudoku
+     */
     @Override
     public String toString() {
         Sudoku sudoku = ((OneChangeStep) Objects.requireNonNull(getLastStep())).getSudoku();
