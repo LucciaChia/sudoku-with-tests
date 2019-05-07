9fa1695946273cc83c4041390d0e8ed7f1921cc1
boris.krul@exxeta.sk <boris.krul@exxeta.sk>
2019-05-07 10:04:11 +0200

    add comment for classes Step and OneChangeStep #11

--- src/main/java/sudoku/step/OneChangeStep.java
+++ src/main/java/sudoku/step/OneChangeStep.java
@@ -9,6 +9,10 @@ import java.util.Map;
 
 import static sudoku.ANSIColour.*;
 
+/**
+ * Class representing single step containing information about current state of the sudoku,
+ * strategy that was used in this step and changes that were made
+ */
 public class OneChangeStep implements Step {
 
     public static int stepNumber = 0;
@@ -19,6 +23,13 @@ public class OneChangeStep implements Step {
     private Map<int[], Integer> deletedPossibilitiesWithLocation;
     private Resolvable resolvable;
 
+    /**
+     * Constructor method which use the most basic information required for step - current state of sudoku
+     * and strategy that will be used in this step
+     *
+     * @param strategy  Resolvable that will be used as strategy in this step
+     * @param sudoku    sudoku that is current state of the sudoku
+     */
     public OneChangeStep(Resolvable strategy, Sudoku sudoku) {
         this.resolvable = strategy;
         this.sudoku = sudoku;
@@ -32,12 +43,27 @@ public class OneChangeStep implements Step {
         this.resolvable = resolvable;
     }
 
+    /**
+     * Constructor method which use the information required for step - current state of sudoku
+     * and strategy that will be used in this step
+     *
+     * @param sudoku                sudoku that is current state of the sudoku
+     * @param solvingStrategyName   string that is name of the strategy that will be used
+     */
     public OneChangeStep(Sudoku sudoku, String solvingStrategyName) {
         stepNumber++;
         this.sudoku = sudoku;
         this.solvingStrategyName = solvingStrategyName;
     }
 
+    /**
+     * Constructor method which use the information required for step - current state of sudoku
+     * and strategy that will be used in this step and the cell that will be affected.
+     *
+     * @param sudoku                sudoku that is current state of the sudoku
+     * @param solvingStrategyName   string that is name of the strategy that will be used
+     * @param cell                  a cell that is affected in this step
+     */
     public OneChangeStep(Sudoku sudoku, String solvingStrategyName, Cell cell) {
         stepNumber++;
         this.sudoku = sudoku;
@@ -45,6 +71,16 @@ public class OneChangeStep implements Step {
         this.cell = cell;
     }
 
+    /**
+     * Constructor method which use the information required for step - current state of sudoku
+     * and strategy that will be used in this step and the cell that will be affected
+     *
+     * @param sudoku                                sudoku that is current state of the sudoku
+     * @param solvingStrategyName                   string that is name of the strategy that will be used
+     * @param cell                                  cell of the pair of cells used in the strategy
+     * @param partnerCell                           cell of the pair of cells used in the strategy
+     * @param deletedPossibilitiesWithLocation      map of possibilities that are deleted in this step
+     */
     public OneChangeStep(Sudoku sudoku, String solvingStrategyName, Cell cell, Cell partnerCell, Map<int[], Integer> deletedPossibilitiesWithLocation) {
         stepNumber++;
         this.sudoku = sudoku;
@@ -79,6 +115,12 @@ public class OneChangeStep implements Step {
         return deletedPossibilitiesWithLocation;
     }
 
+    /**
+     * Method that prints changes achieve by the use of NakedSingleCell or HiddenSingleCell strategy
+     * and then prints the current state of the sudoku
+     *
+     * @param cell  a cell whose actual value has been set in this step
+     */
     @Override
     public void printStep(Cell cell) {
         System.out.println(stepNumber + ". " + solvingStrategyName);
@@ -88,6 +130,14 @@ public class OneChangeStep implements Step {
 //        sudoku.printPossibilitiesInSudoku();
     }
 
+    /**
+     * Method that prints information about possibilities removed by the use of PointingPairsInCell strategy
+     * and then prints current state of the sudoku
+     *
+     * @param cell                              cell of the pair of cells used in the strategy
+     * @param partnerCell                       cell of the pair of cells used in the strategy
+     * @param deletedPossibilitiesWithLocation  possibilities that has been removed in this step
+     */
     @Override
     public void printStepPointingPair(Cell cell, Cell partnerCell, Map<int[], Integer> deletedPossibilitiesWithLocation) {
         System.out.println(stepNumber + ". " + solvingStrategyName);
@@ -100,6 +150,9 @@ public class OneChangeStep implements Step {
         sudoku.toString();
     }
 
+    /**
+     * Method that prints information that Backtracking strategy has been used and prints solved sudoku
+     */
     @Override
     public void printBacktrack() {
         System.out.println(stepNumber + ". " + solvingStrategyName);
@@ -108,6 +161,11 @@ public class OneChangeStep implements Step {
         sudoku.toString();
     }
 
+    /**
+     * method that calls print method for the strategy used in this step
+     *
+     * @return      string that is empty
+     */
     @Override
     public String toString() {
         if (this.getSolvingStrategyName().equals("0: NackedSingleInACell") || this.getSolvingStrategyName().equals("1: HiddenSingleInACell")) {
