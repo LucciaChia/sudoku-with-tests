967aaf6f2506ec69462e965122e7d27739276c55
LucciaChia <krausovalucia23@gmail.com>
2019-05-07 16:05:09 +0200

    #23 add strategy factory

--- src/main/java/sudoku/strategy/StrategyFactory.java
+++ src/main/java/sudoku/strategy/StrategyFactory.java
@@ -1,9 +1,22 @@
 package sudoku.strategy;
 
+/**
+ * hides strategy and their creations
+ */
+
 public class StrategyFactory {
 
-  Resolvable createPointing(){
+  public Resolvable createNackedSingleInACellStrategy(){
+    return new NakedSingleInACell();
+  }
+  public Resolvable createHiddenSingleInACellStrategy(){
+    return new HiddenSingleInACell();
+  }
+  public Resolvable createPointingPairsInCellStrategy(){
     return new PointingPairsInCell();
+  }
+  public Resolvable createBacktrackStrategy(){
+    return new BacktrackLucia();
   }
 
 }
