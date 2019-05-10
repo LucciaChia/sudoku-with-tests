610907d3e59066aae20808bd5171d260b8c3532d
boris.krul@exxeta.sk <boris.krul@exxeta.sk>
2019-05-10 14:54:31 +0200

    update imports in AutomatedInvokerTest

--- src/test/java/sudoku/invoker/AutomatedInvokerTest.java
+++ src/test/java/sudoku/invoker/AutomatedInvokerTest.java
@@ -1,6 +1,6 @@
 package sudoku.invoker;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import sudoku.command.AutomatedInvoker;
 import sudoku.command.Command;
 import sudoku.command.CommandPicker;
